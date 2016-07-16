309cd34f1275bce42bce5a32931494b5aee0db7d
Marcos de la Calle <marcos.dlcs@gmail.com>
2016-07-16 20:38:56 +0200

    Third assignment

---
+++ week-3/services.js
@@ -0,0 +1,76 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+  .constant('baseURL', 'http://localhost:3000/')
+  .service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    return $resource(baseURL + "dishes/:id", null, {
+      'update': {
+        method: 'PUT'
+      }
+    });
+
+  }])
+
+  .factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    return $resource(baseURL + "leadership/:id", null, {'update': {method: 'PUT'}});
+
+  }])
+
+  .factory('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    return $resource(baseURL + "feedback/:id", null, {'update': {method: 'PUT'}});
+
+  }])
+
+  .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    var favFac = {};
+    var favorites = [];
+
+    favFac.addToFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index)
+          return;
+      }
+      favorites.push({id: index});
+    };
+
+    favFac.deleteFromFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index) {
+          favorites.splice(i, 1);
+        }
+      }
+    }
+
+    favFac.getFavorites = function () {
+      return favorites;
+    };
+
+    return favFac;
+  }])
+
+  .factory('$localStorage', ['$window', function ($window) {
+    return {
+      store: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key, defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
+  }])
+
+  .factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    return $resource(baseURL + "promotions/:id");
+
+  }])
+
+;
